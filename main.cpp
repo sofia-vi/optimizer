48859c5ea026fce8b606618ea0ebe5275706b646
sofyv <sofyvi67@gmail.com>
2023-04-19 00:42:31 +0200

    test 3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <fstream>
+
+
 
 int main() {
+    using json = nlohmann::json;
 
+    std::ifstream i(R"(C:\Users\sofyv\CLionProjects\untitled1\JSON\q1.json)");
+    json data = json::parse(i);
+    std::cout << data.contains("name") << std::endl;
     std::cout<< "Hello, World!" << std::endl;
     return 0;
 
